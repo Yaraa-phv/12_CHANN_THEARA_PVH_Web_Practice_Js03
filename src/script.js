1f1fce2c77d8c8bd4b35d9dcb2da5ed25e969913
Theara <thearachann134@gmail.com>
2025-02-15 16:56:08 +0700

    second commit for web practice 03

--- src/script.js
+++ src/script.js
@@ -1,21 +1,20 @@
+let taskArray = []; 
 document.querySelector("#submitForm").addEventListener("submit", (e) => {
-  e.preventDefault(); // Prevent form submission
+  e.preventDefault(); 
 
   const taskInputEL = document.querySelector("#task-input").value.trim();
   const datePickerEL = document.querySelector("#datepicker-actions").value;
   const priorityPickingEL = document.querySelector("#prio-picking").value;
   const taskListEL = document.querySelector("#taskList");
 
-  // Error message element
+
   const errorMessage = document.querySelector("#errorMsg");
-  errorMessage.innerText = ""; // Clear previous error message
+  errorMessage.innerText = "";
 
-  // Validation: Check if fields are empty
   if (taskInputEL === "" || datePickerEL === "" || priorityPickingEL === "Select Priority") {
-    errorMessage.innerText = "Please fill in all the fields!";
+    errorMessage.innerText = "Please fill the information here!!!";
     return;
-  }
-
+  }else{
   const row = document.createElement("tr");
   row.classList.add("bg-white", "border-b", "dark:bg-gray-800", "dark:border-gray-700", "border-gray-200", "dark:text-black");
 
@@ -29,6 +28,15 @@ document.querySelector("#submitForm").addEventListener("submit", (e) => {
     `;
 
   taskListEL.appendChild(row);
-
+}
   document.querySelector("#submitForm").reset();
 });
+
+function toggleStatus(index) {
+  taskArray[index].status = taskArray[index].status === "Pending" ? "Completed" : "Pending";
+  row();
+}
+
+function deleteTask(index) {
+  taskArray.splice(index, 1);
+  row()};
